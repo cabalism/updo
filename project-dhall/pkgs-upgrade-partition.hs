84f18cba77ae62b035ed69881f1bad50daa26d41
Phil de Joux <philderbeast@gmail.com>
2023-02-02 16:54:13 -0500

    Put all pkgs into the upgrade TODO list to start.
    Run code-style-checks --fix on scripts.
    Rerun make project-files.

--- project-dhall/pkgs-upgrade-partition.hs
+++ project-dhall/pkgs-upgrade-partition.hs
@@ -10,26 +10,25 @@
 {-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 
-import Data.List ((\\), intersect)
-import Dhall (Text, Generic, FromDhall, ToDhall, input, auto, embed, inject)
-import Dhall.Core (pretty)
-import System.Environment (getArgs)
+import Data.List (intersect, (\\))
 import qualified Data.Text as T (pack)
 import qualified Data.Text.IO as T (putStrLn)
+import Dhall (FromDhall, Generic, Text, ToDhall, auto, embed, inject, input)
+import Dhall.Core (pretty)
+import System.Environment (getArgs)
 
-data PkgUpgrade =
-    PkgUpgrade
-        { pkgs :: [Text]
-        , done :: [Text]
-        , todo :: [Text]
-        }
-    deriving (Show, Generic, ToDhall, FromDhall)
+data PkgUpgrade = PkgUpgrade
+  { pkgs :: [Text],
+    done :: [Text],
+    todo :: [Text]
+  }
+  deriving (Show, Generic, ToDhall, FromDhall)
 
 main :: IO ()
 main = do
-    file : _ <- getArgs
-    pkgs :: [Text] <- input auto (T.pack file)
-    remaining :: [Text] <- input auto "./project-dhall/pkgs-upgrade-todo.dhall"
-    let todo = pkgs `intersect` remaining
-    let done = pkgs \\ todo
-    T.putStrLn . pretty $ embed inject PkgUpgrade{..}
+  file : _ <- getArgs
+  pkgs :: [Text] <- input auto (T.pack file)
+  remaining :: [Text] <- input auto "./project-dhall/pkgs-upgrade-todo.dhall"
+  let todo = pkgs `intersect` remaining
+  let done = pkgs \\ todo
+  T.putStrLn . pretty $ embed inject PkgUpgrade {..}
