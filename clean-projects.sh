c036818e6d8e8a7df0d79e389a89b2aa6b2b9a5e
Phil de Joux <philderbeast@gmail.com>
2023-02-16 07:27:36 -0500

    Simplify clean scripts.
    Co-authored-by: Neil Mayhew <neil@fpcomplete.com>

--- clean-projects.sh
+++ clean-projects.sh
@@ -1,9 +1,2 @@
 #!/bin/sh
-set -eu
-
-for f in ghc-*.stack.* ghc-*.dhall2cabal.* ghc-*.dhall2config.* ghc-*.dhall2stack.* ghc-*.stack2cabal.* ghc-*.cabal2stack.*
-do
-    if [ -e "${f}" ];
-        then rm "${f}" || true;
-    fi
-done
+rm -f ghc-*.stack.* ghc-*.dhall2cabal.* ghc-*.dhall2config.* ghc-*.dhall2stack.* ghc-*.stack2cabal.* ghc-*.cabal2stack.*
